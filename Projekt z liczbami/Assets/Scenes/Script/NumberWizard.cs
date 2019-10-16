90e744e73c1ad918ea33d37fbe0ed3dd07963184
qwerty <dawidplx1221@gmail.com>
2019-10-16 09:52:31 +0200

    Added new function

--- Projekt z liczbami/Assets/Scenes/Script/NumberWizard.cs
+++ Projekt z liczbami/Assets/Scenes/Script/NumberWizard.cs
@@ -4,29 +4,40 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int min = 1;
+    int max = 500;
+    int guess = 500;
     // Start is called before the first frame update
     void Start()
     {
         print("Witaj u Liczbowego Czarodzeija");
         print("Pomysl jakas liczbe w swojej glowie ale jej nie mow");
-        int min = 1;
-        int max = 500;
         print("Najwieksza liczba jaka mozesz wybrac jest"+max);
         print("Najmniensza licazba jaka mozesz wybrac to" +min);
-        print("Czy twoja liczba jest wiekasza czy mniejsza od 500?");
         print("Aby odpowiedziec na pyanie nacisnij: Gorna strzalka = wyzsza liczba, Dolna liczba = nizsza liczba");
-        if (Input.GetKeyDown(KeyCode.UpArrow)) ;
-        print("Gorna strzalka zostala nacisnieta");
-        if (Input.GetKeyUp(KeyCode.DownArrow)) ;
-        print("Dolna strzalak zostala nacisnieta");
-        if (Input.GetKeyDown(KeyCode.Return)) ;
-            //CTRL + ALT + M , CTRL + H 
+
+            //CTRL + ALT + M , CTRL + H = nakladka unity (wyszukiwarka) 
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            guess = (min + max) / 2;
+            print("Czy twoja liczba jest wiekasza lub mniejsza niz " + guess);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            max = guess;
+            guess = (min + max) / 2;
+            print("Czy twoja liczba jest wiekasza lub mniejsza niz " + guess);
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("haha zgadlem");
+        }
     }
 }
