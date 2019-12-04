3712a5936bd36e2790d12dc15cc49bbe73961541
qwerty <dawidplx1221@gmail.com>
2019-12-04 09:56:41 +0100

    New C++ project

---
+++ C++/TripleX.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    //Print welcom messages to the terminal 
+std:: cout << "You are a secret agent breaking a secure server room...";
+std:: cout << std::endl;
+std:: cout << "Enter the code to continue...";
+
+// Declere 3 number code 
+const int a = 4;
+const int b = 3;
+const int c = 2 ;
+
+const int sum = a + b + c;
+const int product = a * b * c ;
+
+// Print sum and product to the terminal 
+std::cout << std::endl;
+std::cout << sum << std::endl;
+std::cout << product << std::endl;
+
+return 0;
+}
